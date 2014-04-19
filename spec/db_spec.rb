25d56dcd6a7b6c78bc6405c5eaf2d4dd228ccd40
Ace Suares <ace@suares.an>
2014-04-19 07:28:11 -0400

    clean up test

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -5,45 +5,21 @@ ActiveRecord::Base.establish_connection(
     :database  => "db/database.db"
 )
 # added this to test quickly
-unless ActiveRecord::Base.connection.table_exists? 'apartments'
-  puts '=====>>>>>Creating new table !!!<======='.green
-  ActiveRecord::Migration.create_table :apartments do |t|
-    t.string        :name
-    t.string        :description
-    t.binary        :picture
+ActiveRecord::Migration.drop_table :apartments if ActiveRecord::Base.connection.table_exists? 'apartments'
 
-    t.timestamp
-
-    t.string  :identifier
-    t.string  :original_filename
-    t.string  :content_type
-    t.integer :size
-    t.binary  :data
-
-  end
-
-  ActiveRecord::Migration.create_table :persoons do |t|
+ActiveRecord::Migration.create_table :apartments do |t|
   t.string        :name
-    t.string        :description
+  t.string        :description
   t.binary        :picture
 
-  t.timestamp
+  t.timestamps
 
   t.string  :identifier
   t.string  :original_filename
   t.string  :content_type
   t.integer :size
   t.binary  :data
-
-  end
 end
-
-class PersoonUploader < CarrierWave::Uploader::Base
-
-  storage :db
-  store_dir :persoons
-end
-
 
 class ApartmentUploader < CarrierWave::Uploader::Base
   storage :db
@@ -54,50 +30,5 @@ class Apartment < ActiveRecord::Base
   mount_uploader :picture, ApartmentUploader
 end
 
-class Persoon < ActiveRecord::Base
-  mount_uploader :picture, PersoonUploader
-end
-
-
-
-ap = Apartment.new
 ap = FactoryGirl.create(:apartment)
 
-pr = Persoon.new
-pr = FactoryGirl.create(:persoon)
-
-
-
-# puts ap.tellme
-# puts FactoryGirl.methods.sort
-# puts CarrierWave::Storage::DB::File
-# puts a.methods.sort
-# a.store!(f)
-# puts a.methods.sort
-# puts a.class
-# puts ApartmentUploader.methods.sort
-
-# apartments = ApartmentUploader.new
-# apartments.picture = File.open('spec/fixtures/images/blue-curacao.jpg')
-# apartments.save!
-# puts ApartmentUploader.methods.sort
-# @upload CarrierWave::Storage::DB.new
-
-# describe Apartment do
-# it "insert a Apartment without attachment and retrieve it back" do
-# subject.name = 'Nechi kas'
-# subject.picture = @uploader
-# subject.save!
-# Apartment.find(1).name.should eq('Nechi kas')
-# end
-
-# it "insert a Apartment with attachment and retrieve it back" do
-# subject.name = 'Nechi kas'
-# subject.picture = @uploader.store! 'spec/fixtures/blue-curacao.jpg'
-# subject.save!
-#     
-# Apartment.find(1).name.should eq('Nechi kas')
-# 
-# end
-
-# end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :apartment do
     name "Curacao_Apartment"
     description  "Nice Apartment near the ocean good for drinking Blue-Curacao"
-    picture fixture_file_upload('spec/fixtures/images/blue-curacao.jpg', 'image/jpg')  
+    picture fixture_file_upload('spec/fixtures/images/blue-curacao.jpg')  
  end
 end
